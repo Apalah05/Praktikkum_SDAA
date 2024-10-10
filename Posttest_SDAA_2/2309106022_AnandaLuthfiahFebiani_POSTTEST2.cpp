5a20051498e8e6cd0ef62d2edd430e0af208a8c7
Apalah05 <162230260+Apalah05@users.noreply.github.com>
2024-10-10 20:10:35 +0800

    Update 2309106022_AnandaLuthfiahFebiani_POSTTEST2.cpp

--- Posttest_SDAA_2/2309106022_AnandaLuthfiahFebiani_POSTTEST2.cpp
+++ Posttest_SDAA_2/2309106022_AnandaLuthfiahFebiani_POSTTEST2.cpp
@@ -7,21 +7,10 @@ using namespace std;
 struct HasilTambang {
     string nama;
     int jumlah;
-    HasilTambang* next;
 };
 
 HasilTambang dataTambang[MAX];
 int tambang = 0;
-
-HasilTambang* head = nullptr;
-
-HasilTambang* Linked(string nama, int jumlah) {
-    HasilTambang* baru = new HasilTambang();
-    baru->nama = nama;
-    baru->jumlah = jumlah;
-    baru->next = nullptr;
-    return baru;
-}
 
 void Tampil() {
     if (tambang > 0) {
@@ -33,7 +22,6 @@ void Tampil() {
         cout << "=== Data Tambang Kosong ===\n";
     }
 }
-
 void Tambah() {
     if (tambang < MAX) {
         cin.ignore();
@@ -45,40 +33,7 @@ void Tambah() {
     } else {
         cout << "<-- GUDANG PERTAMBANGAN TELAH PENUH -->\n";
     }
-}
-
-void TambahAwalAkhir(string nama, int jumlah) {
-    HasilTambang* baru = Linked(nama, jumlah);
-    baru->next = head;
-    if (head == nullptr) {
-        head = baru;
-        } else {
-            HasilTambang* temp = head;
-            while (temp->next != nullptr) {
-                temp = temp->next;
-            }
-            temp->next = baru;
-        }
-        head = baru;
-    }
-
-void posisilinked(string nama, int jumlah, int position) {
-    HasilTambang* baru = Linked(nama, jumlah);
-    if (position == 1) {
-        baru->next = head;
-        head = baru;
-        return;
-    }
-    HasilTambang* temp = head;
-    for (int i = 0; i < position - 1 && temp != nullptr; i++) {
-        temp = temp->next;
-    }
-    if (temp == nullptr) {
-        cout << "Posisi hasil tambang tidak ada\n" << endl;
-    } else {
-        baru->next = temp->next;
-        temp->next = baru;
-    }
+    Tampil();
 }
 
 void Ubah() {
@@ -93,6 +48,7 @@ void Ubah() {
         cout << "Ubah Jumlah Hasil Tambang Menjadi: ";
         cin >> dataTambang[y - 1].jumlah;
     }
+    Tampil();
 }
 
 void Hapus() {
@@ -106,24 +62,17 @@ void Hapus() {
         }
         tambang--;
     }
+    Tampil();
 }
 
-void HitungNode() {
-    int hitung = 0;
-    HasilTambang* temp = head;
-    while (temp != nullptr) {
-        hitung++;
-        temp = temp->next;
-    }
-}
-
-void HasilLinked() {
-    HasilTambang* temp = head;
-    while (temp != nullptr) {
-        cout << temp->nama << "(Jumlah: " << temp->jumlah << ") -> " << endl;
-        temp = temp->next;
-    }
-    cout << "NULL" << endl;
+bool Login() {
+    string Username, Password;
+    cout << "=== Login ===\n";
+    cout << "Username: ";
+    getline(cin, Username);
+    cout << "Password: ";
+    getline(cin, Password);
+    return Username == "admin" && Password == "1234";
 }
 
 int main() {
@@ -136,10 +85,8 @@ int main() {
              << "2. Ubah Data Hasil Tambang\n"
              << "3. Hapus Data Hasil Tambang\n"
              << "4. Tampilkan Data Hasil Tambang\n"
-             << "5. Linked Hasil Tambang\n"
-             << "6. Hitung Total Linked Hasil Tambang\n"
-             << "7. Keluar\n"
-             << "Pilih menu yang tertera diatas (1/2/3/4/5/6/7): ";
+             << "5. Keluar\n"
+             << "Pilih menu yang tertera diatas (1/2/3/4/5): ";
         cin >> pilihan;
 
         switch (pilihan) {
@@ -155,13 +102,7 @@ int main() {
             case 4: 
                 Tampil(); 
                 break;
-            case 5:
-                HasilLinked();
-                break;
-            case 6:
-                HitungNode();
-                break;
-            case 7:
+            case 5: 
                 cout << "Terima kasih telah mendata Hasil Tambang dari pertambangan\n"; 
                 return 0;
             default: 
